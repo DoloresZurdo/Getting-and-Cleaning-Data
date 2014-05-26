5a5f3517743906613f094e07acd7dc5327dfed72
Dolores Zurdo <dzcnekane@gmail.com>
2014-05-26 12:42:55 +0200

    v1.0

---
+++ CodeBook.R
@@ -0,0 +1,33 @@
+CodeBook
+
+Read all original data:
+origin_activity_label: activity of the subject
+   1 - Walking
+   2 -  Walking_upstairs
+   3 - Walking downstairs
+   4 - Sitting
+   5 - Standing
+   6  - Laying
+
+origin_features: data of acceleration and ngular velocity for "x", "y", and "z" axis
+
+Test data
+origin_subject_test: subject of test data
+origintest_X_data: data of features
+origintest_Y_data: data of activity of the subject
+
+Train data
+origin_subject_train: subject of train data
+origintrain_X_data: data of features
+origintrain_Y_data: data of activity of the subject
+
+Merged data (test and train data)
+Subject_data 
+X_data
+Y_data
+X_data_select - only data of features which have a mean or a standar desviation
+
+Merge all data (subject, X and Y data in one file)
+data
+
+Mean_data: data with the correspondent mean
